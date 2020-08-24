747bc2faec065ef68102d43cfc89327f00448af0
github username <github email address>
2020-08-24 17:06:46 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
-// your Bomb code here!
+import React from 'react'
+ 
+ class Bomb extends React.Component{
+ 
+    constructor(props) {
+        super()
+        this.state = {secondsLeft: props.initialCount}
+      }
+
+    countDown=()=>{
+        setInterval(()=>{
+
+            let currentCount = this.state.secondsLeft
+    
+                let newCount = currentCount-1
+                this.setState({secondsLeft:newCount})
+
+        }, 1000)
+    }
+
+     render(){
+         return(
+             <h2 onClick ={this.countDown}>{this.state.secondsLeft > 0? `${this.state.secondsLeft} seconds left before I go boom!`:"Boom!"}</h2>
+         )
+        }
+    }
+
+    
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,17 @@
-// your ImageSlider code here!
+ import React from 'react'
+
+ 
+ class ImageSlider extends React.Component{
+
+    state= {currentSlideIndex:0}
+
+     render(){
+         return(
+         <h2>I am on slide {this.state.currentSlideIndex}</h2>
+         )
+        }
+    }
+    
+    
+export default ImageSlider
+
